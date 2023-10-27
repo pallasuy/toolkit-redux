d96b33c9f431461b44f4a874a2063e19f563c080
pallasuy <pallas5192@gmail.com>
2023-10-27 11:46:22 -0300

    FEATURE: Generando lista de pokemons y botones de paginacion

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,12 +1,27 @@
 import { useEffect } from "react"
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getPokemons } from "./store/slices/pokemon";
+import { store } from './store/'
 
 export const PokemonApp = () => {
 
     //Si quiero que algo se ejecute una unica vez utilizo useEffect
 
     const dispatch = useDispatch();
+    const { isLoading = false, page = 1, pokemons = [] } = useSelector(state => state.pokemons);//UseSelector para que se vuelva a renderizar el componente
+
+    const handlePreviousClick = (page) => {
+        console.log(page);
+        const nextPage = (page - 1) > 1 ? (page - 1) : 1;
+
+        dispatch(getPokemons(nextPage));
+    };
+
+    const handleNextClick = (page) => {
+        console.log(page);
+        const nextPage = page + 1;
+        dispatch(getPokemons(nextPage));
+    };
 
     useEffect(() => {
         dispatch(getPokemons());
@@ -18,11 +33,33 @@ export const PokemonApp = () => {
         <>
             <h1>PokemonApp</h1>
             <hr />
+            <span> Loading:  {isLoading ? 'True' : 'False'} </span>
+            <hr />
+
             <ul>
-                <li>Hola</li>
-                <li>Hola</li>
-                <li>Hola</li>
+                {pokemons.map((pokemon) => {
+                    return <li key={pokemon.name}> {pokemon.name} ( {pokemon.url} ] </li>
+                })
+                }
             </ul>
+
+            <button
+                disabled={isLoading || page <= 1}
+                onClick={() => {
+                    handlePreviousClick(page)
+                }}
+            >
+                Previous
+            </button>
+
+            <button
+                disabled={isLoading}
+                onClick={() => {
+                    handleNextClick(page)
+                }}
+            >
+                Next
+            </button>
         </>
 
     )
--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,29 +1,23 @@
 import { pokemonApi } from "../../../api/pokemonApi";
 import { setPokemons, startLoadingPokemons } from "./pokemonSlice"
 
-export const getPokemons = (page = 0) => {
+export const getPokemons = (page = 1) => {
     return async (dispatch, getState) => {
         dispatch(startLoadingPokemons());
         //TODO: realizar peticion http
 
+        const offset = (page - 1) * 10;
 
         //utilizando fetch: 
 
         /*  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`);
          const data = await response.json(); */
 
+
         //utilizandio axios 
+        const { data } = await pokemonApi.get(`/pokemon?limit=10&offset=${offset}`);
 
-        const { data } = await pokemonApi.get(`/pokemon?limit=10&offset=${page * 10}`);
-
-
-        dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
-
-
-
-
-
-
+        dispatch(setPokemons({ pokemons: data.results, page: page }));
         //dispatch(setPokemons());
     }
 
